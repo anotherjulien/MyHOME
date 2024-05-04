bba3c6420351c52d3139b1606f3a7157183d407e
andrea <andrea.parisi@gmail.com>
2024-05-04 10:42:47 +0200

    Add the support to move a cover to a position without advanced device

--- custom_components/myhome/cover.py
+++ custom_components/myhome/cover.py
@@ -35,7 +35,7 @@ from .const import (
 from .myhome_device import MyHOMEEntity
 from .gateway import MyHOMEGatewayHandler
 from datetime import datetime
-
+import asyncio
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     if PLATFORM not in hass.data[DOMAIN][config_entry.data[CONF_MAC]][CONF_PLATFORMS]:
@@ -110,6 +110,9 @@ class MyHOMECover(MyHOMEEntity, CoverEntity):
 
         self._interface = interface
         self._full_where = f"{self._where}#4#{self._interface}" if self._interface is not None else self._where
+        self._attr_opening_time = opening_time
+        self._attr_closing_time = closing_time
+        self._attr_advanced = advanced
 
         self._attr_supported_features = CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE | CoverEntityFeature.STOP
         if advanced:
@@ -130,8 +133,6 @@ class MyHOMECover(MyHOMEEntity, CoverEntity):
         self._attr_is_closing = None
         self._attr_is_closed = None
         self._attr_last_event = datetime.now()
-        self._attr_opening_time = opening_time
-        self._attr_closing_time = closing_time
 
     async def async_update(self):
         """Update the entity.
@@ -152,7 +153,38 @@ class MyHOMECover(MyHOMEEntity, CoverEntity):
         """Move the cover to a specific position."""
         if ATTR_POSITION in kwargs:
             position = kwargs[ATTR_POSITION]
-            await self._gateway_handler.send(OWNAutomationCommand.set_shutter_level(self._full_where, position))
+            if self._attr_advanced:
+                await self._gateway_handler.send(OWNAutomationCommand.set_shutter_level(self._full_where, position))
+            elif self._attr_opening_time > 0 or self._attr_closing_time > 0:
+
+                if self._attr_is_closing or self._attr_is_closing:
+                    await self._gateway_handler.send(OWNAutomationCommand.stop_shutter(self._full_where))
+
+                if self._attr_current_cover_position is None:
+                    return                
+
+                required_move = int(position - self._attr_current_cover_position)
+                if required_move > 0:
+                    """ open """
+                    required_time = abs(self._attr_opening_time * required_move / 100)
+                    LOGGER.debug(
+                        "Open -> Required time %s",
+                        required_time,
+                    )
+                    await self._gateway_handler.send(OWNAutomationCommand.raise_shutter(self._full_where))
+                    await asyncio.sleep(required_time)
+                    await self._gateway_handler.send(OWNAutomationCommand.stop_shutter(self._full_where))
+                elif required_move < 0:
+                    """ close """
+                    required_time = abs(self._attr_closing_time * required_move / 100)
+                    LOGGER.debug(
+                        "Close -> Required time %s",
+                        required_time,
+                    )
+                    await self._gateway_handler.send(OWNAutomationCommand.lower_shutter(self._full_where))
+                    await asyncio.sleep(required_time)
+                    await self._gateway_handler.send(OWNAutomationCommand.stop_shutter(self._full_where))
+
 
     async def async_stop_cover(self, **kwargs):  # pylint: disable=unused-argument
         """Stop the cover."""
@@ -182,7 +214,7 @@ class MyHOMECover(MyHOMEEntity, CoverEntity):
                     elif self._attr_current_cover_position is not None:
                         self._attr_current_cover_position = round(max(0, self._attr_current_cover_position - (100 * elapsed_seconds / self._attr_closing_time)), 0)
 
-            LOGGER.info(
+            LOGGER.debug(
                 "%s %s",
                 self._gateway_handler.log_id,
                 self._attr_current_cover_position,
